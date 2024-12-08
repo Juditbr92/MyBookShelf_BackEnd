b23c7c93cf4b2ee5ed13b4bca7b5be7c157bd532
judit Bardon <juditbardonromero@gmail.com>
2024-12-08 18:02:58 +0000

    modified book controller so it uses the user_id

--- src/controller/book.controller.js
+++ src/controller/book.controller.js
@@ -19,8 +19,19 @@ async function getBooksUser(req, res){
     const connection = await connectionPromise;
     try{
         const user_id = req.query.user_id
+
+        if(!user_id) {
+            return res.status(400).send({error: true, message:"User ID required"})
+        }
+
         const sql = `SELECT * FROM mybookshelf.books WHERE books.user_id=?`
         let [result] = await connection.query(sql, [user_id])
+
+        // Si no hay libros devolvemos mensaje vacio:
+        if(result.length === 0){
+            return res.status(404).send({error: false, message: "There are no books for this user", books: []});
+        }
+
         res.send(result);
         console.log(result)
     }
@@ -37,6 +48,9 @@ async function addBook(req, res){
     const connection = await connectionPromise
     try{
         const {title, author, type, photo, rating, notes, user_id} = req.body;
+        if (!title || !author || !type || typeof rating !== 'number') {
+            return res.status(400).json({ error: 'Invalid data' });
+        }
         await connection.query('INSERT INTO books (title, author, type, photo, rating, notes, user_id) VALUES (?, ?, ?, ?, ?, ?, ?)', [title, author, type, photo, rating, notes, user_id])
         res.status(201).send({message: 'Book successfully added to your BookShelf'})
     } catch(error){
